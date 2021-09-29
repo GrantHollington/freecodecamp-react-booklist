9dbf0783abf67a74b0200796339ee7590e587242
GrantHollington <grant.j.hollington@gmail.com>
2021-09-29 12:26:54 +1000

    add basic css

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// import CSS
+import './index.css'
+
 function BookList() {
   return (
-    <section>
+    <section className="booklist">
       <Book />
       <Book />
       <Book />
@@ -14,7 +17,7 @@ function BookList() {
 
 const Book = () => {
   return (
-  <article>
+  <article className="book">
     <Image />
     <Title />
     <Author />
@@ -22,10 +25,10 @@ const Book = () => {
   );
 }
 
+// hard coded components
 const Image = () => (
 <img src="https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9781/7812/9781781251492.jpg" alt="The Obstacle is the Way" />
 );
-
 const Title = () => <h1>The Obstacle is The Way</h1>
 const Author = () => <h4>Ryan Holiday</h4>
 ReactDOM.render(<BookList />, document.getElementById('root'))
