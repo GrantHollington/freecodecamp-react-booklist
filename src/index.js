46504f098ecdd5262fda9f04148ef39be9a64e8b
GrantHollington <grant.j.hollington@gmail.com>
2021-09-29 22:30:35 +1000

    add javascript within jsx

--- src/index.js
+++ src/index.js
@@ -8,27 +8,23 @@ function BookList() {
   return (
     <section className="booklist">
       <Book />
-      <Book />
-      <Book />
-      <Book />
     </section>
   ); 
 }
 
+const author = 'Ryan Holiday'
 const Book = () => {
+  const title = 'The Obstacle is the Way';
   return (
   <article className="book">
-    <Image />
-    <Title />
-    <Author />
+    <img src="https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9781/7812/9781781251492.jpg" alt="The Obstacle is the Way" />
+    <h1>{title}</h1>
+    <h4>{author.toUpperCase()}</h4>
+    {/* <p>{lex x = 6}</p> */}
+    <p>{6+8}</p>
   </article>
   );
 }
 
-// hard coded components
-const Image = () => (
-<img src="https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9781/7812/9781781251492.jpg" alt="The Obstacle is the Way" />
-);
-const Title = () => <h1>The Obstacle is The Way</h1>
-const Author = () => <h4 style={{color: '#617d98', fontSize: '0.75rem', marginTop: '0.25rem'}}>Ryan Holiday</h4>
+
 ReactDOM.render(<BookList />, document.getElementById('root'))
