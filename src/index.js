78d7050037cdf3332f0dd6227f4146f04ec91be8
GrantHollington <grant.j.hollington@gmail.com>
2021-09-29 12:07:45 +1000

    addd hard coded components

--- src/index.js
+++ src/index.js
@@ -5,12 +5,27 @@ function BookList() {
   return (
     <section>
       <Book />
+      <Book />
+      <Book />
+      <Book />
     </section>
   ); 
 }
 
 const Book = () => {
-  return <article>This is a car</article>
+  return (
+  <article>
+    <Image />
+    <Title />
+    <Author />
+  </article>
+  );
 }
 
+const Image = () => (
+<img src="https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9781/7812/9781781251492.jpg" alt="The Obstacle is the Way" />
+);
+
+const Title = () => <h1>The Obstacle is The Way</h1>
+const Author = () => <h4>Ryan Holiday</h4>
 ReactDOM.render(<BookList />, document.getElementById('root'))
