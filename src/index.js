4f718dee1506a0af085c0ab6ada7d2f82c956fd0
GrantHollington <grant.j.hollington@gmail.com>
2021-09-30 11:57:53 +1000

    add book variables and props to index.js

--- src/index.js
+++ src/index.js
@@ -3,25 +3,42 @@ import ReactDOM from 'react-dom';
 
 // import CSS
 import './index.css'
+// setup vars
+const firstBook = {
+  img: "https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9781/7812/9781781251492.jpg",
+  title: 'The Obstacle is the Way',
+  author: "Ryan Holiday"
+};
+const secondBook = {
+  img: "https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9780/7535/9780753555200.jpg",
+  title: 'Zero to One',
+  author: "Peter Thiel"
+}
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
+      <Book 
+        img={firstBook.img} 
+        title={firstBook.title} 
+        author={firstBook.author}
+        />
+      <Book 
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+        />
     </section>
   ); 
 }
 
-const author = 'Ryan Holiday'
-const Book = () => {
-  const title = 'The Obstacle is the Way';
+const Book = (props) => {
+  console.log(props);
   return (
   <article className="book">
-    <img src="https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9781/7812/9781781251492.jpg" alt="The Obstacle is the Way" />
-    <h1>{title}</h1>
-    <h4>{author.toUpperCase()}</h4>
-    {/* <p>{lex x = 6}</p> */}
-    <p>{6+8}</p>
+    <img src={props.img} alt="" />
+    <h1>{props.title}</h1>
+    <h4>{props.author}</h4>
   </article>
   );
 }
