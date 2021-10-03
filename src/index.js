39c47faa828aad3b6e521b2f77c666df9ce1c972
GrantHollington <grant.j.hollington@gmail.com>
2021-10-03 18:12:13 +1100

    moved books inside an array and removed variable for each book

--- src/index.js
+++ src/index.js
@@ -4,45 +4,38 @@ import ReactDOM from 'react-dom';
 // import CSS
 import './index.css'
 // setup vars
-const firstBook = {
+const books = [
+{
   img: "https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9781/7812/9781781251492.jpg",
   title: 'The Obstacle is the Way',
   author: "Ryan Holiday"
-};
-const secondBook = {
+},
+{
   img: "https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9780/7535/9780753555200.jpg",
   title: 'Zero to One',
   author: "Peter Thiel"
 }
-
+];
+const names = ['john', 'peter', 'susan'];
+const newNames = names.map((name) => {
+  return <h1>{name}</h1>;
+});
+console.log(newNames) 
 function BookList() {
   return (
     <section className="booklist">
-      <Book 
-        img={firstBook.img} 
-        title={firstBook.title} 
-        author={firstBook.author}
-        >
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores in voluptatum commodi ullam ab voluptate corporis unde, consectetur illo assumenda placeat, nobis distinctio, alias fugit error quod esse? Illo, amet.</p>
-        </Book>
-      <Book 
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-        />
+      {newNames}
     </section>
   ); 
 }
 
-const Book = ({img, title, author, children}) => {
+const Book = ({img, title, author}) => {
   // const {img, title, author} = props
   return (
   <article className="book">
     <img src={img} alt="" />
     <h1>{title}</h1>
-    <h4>{author}</h4>
-    {children}
-  </article>
+      </article>
   );
 }
 
